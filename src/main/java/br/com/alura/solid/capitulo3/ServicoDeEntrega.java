268d49aabdd51eba6bdcc11ad88a8bbfbce30431
Stephanie dos Santos Amaral <stephanie.amaral@zup.com.br>
2021-07-23 10:16:13 -0300

    Classes abertas, open closed e dependency inversion principle

---
+++ src/main/java/br/com/alura/solid/capitulo3/ServicoDeEntrega.java
@@ -0,0 +1,5 @@
+package br.com.alura.solid.capitulo3;
+
+public interface ServicoDeEntrega {
+	double para(String cidade);
+}
---
+++ src/main/java/br/com/alura/solid/capitulo3/TabelaDePreco.java
@@ -0,0 +1,5 @@
+package br.com.alura.solid.capitulo3;
+
+public interface TabelaDePreco {
+	double descontoPara(double valor);
+}
